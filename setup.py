5c720e289d2a1bb6a0d6b90a7f2686eb1f740783
rupbra <rupbra@outlook.com>
2024-01-04 17:31:14 -0500

    Add .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UsdCadRate',
+    version='V.2',
+    packages=[''],
+    url='',
+    license='',
+    author='Rupert Bramall',
+    author_email='mail@thebramalls.com',
+    description='USD to CAD Exchange Conversion'
+)
